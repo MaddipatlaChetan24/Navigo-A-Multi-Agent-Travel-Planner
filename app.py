acaef3057780518849810f2057bfb168cd4f5cdb
Maddipatla Chatan <chetan121318@gmail.com>
2026-09-18 05:43:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI(
     title="Navigo",
-    description="LangGraph Multi-Agent Travel Planner with FastAPI Frontend",
+    description="LangGraph Multi-Agent Travel Planner with Fasnd",
     version="1.0.0"
 )
 
