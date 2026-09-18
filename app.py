e1baf6db4255f20dcddd9fe3b56f6a230f7efefb
Maddipatla Chatan <chetan121318@gmail.com>
2026-09-18 05:43:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 
-from backend import run_travel_agent
-
-BASE_DIR = Path(__file__).resolve().parent
-
-app = FastAPI(
-    title="Navigo",
-    description="LangGraph Multi-Agent Travel Planner with Fasnd",
-    version="1.0.0"
-)
-
-
 app.mount(
     "/static",
     StaticFiles(directory=str(BASE_DIR / "static")),
