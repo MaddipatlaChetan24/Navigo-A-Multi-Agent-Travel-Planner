8dc62f573ecda493232534247990387d5f039389
Maddipatla Chatan <chetan121318@gmail.com>
2026-09-19 23:00:23 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -10,11 +10,6 @@ os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 
 
 
-    user_query: str
-
-def flight_agent(state: TravelState):
-    query = state["user_query"]
-        "llm_calls": state.get("llm_calls", 0) + 1
 # Hotel Agent
 # =========================
 
